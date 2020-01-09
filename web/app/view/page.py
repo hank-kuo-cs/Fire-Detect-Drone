e4bd5b5f702978b3f6073dbfb54f240ce1cb60c0
hank-kuo-cs <hank.cs04@nctu.edu.tw>
2020-01-10 01:58:44 +0800

    get status error handler

--- web/app/view/page.py
+++ web/app/view/page.py
@@ -24,20 +24,29 @@ def dev():
 
 @view.route('/detect')
 def detect():
-    status_file = 'web/app/data/status.txt'
-
     while True:
-        with open(status_file, 'r') as f:
-            s = f.readlines()
+        try:
+            is_fire, img_name = get_status()
+            break
+        except IndexError:
+            print('index error')
 
-            find_status = re.findall('Fire=(.?)xxxx', s[0])
-            if find_status:
-                is_fire = find_status[0]
-
-                img_name = re.findall('Name=(.+?)\n', s[1])
-                if img_name:
-                    img_name = img_name[0]
-                    break
-    print(img_name)
-    is_fire = (is_fire == '1')
     return render_template('detect.html', img_name=img_name, is_fire=is_fire)
+
+
+def get_status():
+    status_file = 'web/app/data/status.txt'
+    with open(status_file, 'r') as f:
+        s = f.readlines()
+
+        find_status = re.findall('Fire=(.?)xxxx', s[0])
+        if find_status:
+            is_fire = find_status[0]
+
+            img_name = re.findall('Name=(.+?)\n', s[1])
+            if img_name:
+                img_name = img_name[0]
+
+            is_fire = (is_fire == '1')
+
+        return is_fire, img_name
